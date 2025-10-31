16b06f4d12542d359cfcadf28934de4334116ce6
FreeZ3e <53177841+FreeZ3e@users.noreply.github.com>
2025-10-31 13:19:37 +0800

    Add files via upload

---
+++ detection/configs/gmamba/mask_rcnn_vssm_fpn_coco_base.py
@@ -0,0 +1,31 @@
+_base_ = [
+    '../swin/mask-rcnn_swin-t-p4-w7_fpn_1x_coco.py'
+]
+
+model = dict(
+    backbone=dict(
+        type='MM_VSSM',
+        out_indices=(0, 1, 2, 3),
+        pretrained=,  # your path
+        dims=116,
+        depths=(2, 2, 15, 2),
+        ssm_d_state=1,
+        ssm_dt_rank="auto",
+        ssm_ratio=2.0,
+        ssm_conv=3,
+        ssm_conv_bias=False,
+        forward_type="v05_noz", # v3_noz
+        mlp_ratio=4.0,
+        downsample_version="v3",
+        patchembed_version="v2",
+        drop_path_rate=0.3,
+        norm_layer="ln2d",
+    ),
+    neck=dict(in_channels=[116, 232, 464, 928]),
+)
+
+# too big
+# train_dataloader = dict(batch_size=1) # as gpus=16
+
+
+
